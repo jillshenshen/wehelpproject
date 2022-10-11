c6eb4d2590802f8365ef3c166e19a0e23e070924
Jill shen <pinkastro@gmail.com>
2022-10-11 13:58:40 +0800

    add week-4

---
+++ week-4/test.py
@@ -0,0 +1,57 @@
+from crypt import methods
+from email import message
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import session
+from flask import redirect
+from flask import url_for
+app=Flask(__name__,static_folder="public",static_url_path="/")
+app.secret_key="member"
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
+
+@app.route("/signin",methods=["post"])
+def signin():
+    name=request.form["name"]
+    password=request.form["password"]
+    
+    if name=="test" and password=="test":
+       session["username"]=name
+       return redirect("/member")
+    if not name or not password:
+        
+        return redirect("/error?message=123")
+    if name!="test" or password!="test":
+        
+        return redirect("/error?message=456")       
+
+@app.route("/member")
+def member():
+    if "username" in session: 
+        return render_template("member.html")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/error")
+def error():
+    data=request.args.get("message")
+    if data=="123": 
+        return render_template("error.html",text="請輸入帳號,密碼")
+    if data=="456":
+        return render_template("error.html",text="帳號或密碼輸入錯誤")    
+
+@app.route("/signout")
+def signout():
+    session.pop("username",None)
+    return redirect("/")
+  
+
+
+
+
+app.run(port=3000)
